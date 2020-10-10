5040d4e392bf7dde809626fd80def9093c6c15bf
MedaOcean <MedaOcean@users.noreply.github.com>
2020-10-10 13:48:32 +0300

    Added generate_md.py cause 1 operation was wrong

--- leetcode/generate_md.py
+++ leetcode/generate_md.py
@@ -1,14 +1,18 @@
 s=open("in.txt").readlines()[0]
-s_0=s[5:len(s)]
-s_0=s_0.strip()
+q="".join(s)
+q=q.split()
+w=q[1]
+h=q[2]
+j=" "
+k=w+j+h
 s_1=open("in.txt").readlines()[1]
 s_2=open("in.txt").readlines()[2:len(open("in.txt").readlines())]
 s_3=len(s_1)
 s_4=s_3-2
 l=s_1[30:s_4]
 a="# Intervals"
-b='+ [{d}](#{e})'.format(d=s_0,e=l)
-c="## {f}".format(f=s_0)
+b='+ [{d}](#{e})'.format(d=k,e=l)
+c="## {f}".format(f=k)
 p="'''python"
 s_5=''.join(s_2)
 n=s_5.strip('/n')
